afac4ee0f0f5c3ddc6a7b7547a0b5f1b4f1040a4
argrod <aran.garrod@gmail.com>
2020-10-19 20:47:32 +0900

    First group of works for FreeCodeCamp

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("I am learning C#");
+        }
+    }
+}
---
+++ PrimitiveDataTypes/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Variables
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+            string firstName = "Tod";
+            string freeSpace = " ";
+
+            System.Console.WriteLine(firstName + " " + age);
+            System.Console.WriteLine(firstName + freeSpace + age);
+        }
+    }
+}
